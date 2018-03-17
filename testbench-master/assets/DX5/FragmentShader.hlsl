4182344b399c0d2c3b6666a9276586f468b3349f
Unknown <mick3_70@hotmail.com>
2018-03-17 10:16:18 +0100

    FragmentShader: removed old code

--- testbench-master/assets/DX5/FragmentShader.hlsl
+++ testbench-master/assets/DX5/FragmentShader.hlsl
@@ -22,38 +22,3 @@ float4 main(Input input) : SV_TARGET
 	return color;
 }
 
-
-////#include "Defines.h"
-//struct Input
-//{
-//	float4 pos : SV_POSITION;
-//#ifdef NORMALDEF
-//	float4 normal : NORMAL;
-//#endif
-//#ifdef TEXTCOORDDEF
-//	float2  texCoord  : TEXCOORD;
-//#endif
-//};
-//
-//cbuffer DIFFUSE_TINT_NAMEDEF : register(DIFFUSE_TINTDEF)
-//{
-//	float4 diffuseTint;
-//}
-//
-//#ifdef DIFFUSE_SLOTDEF
-//Texture2D DiffuseTexture : register(DIFFUSE_SLOTDEF);
-//SamplerState DiffuseSampler : register(s0);
-//#endif
-//
-//
-//float4 main(Input input) : SV_TARGET
-//{
-//#ifdef DIFFUSE_SLOTDEF
-//	float4 col = DiffuseTexture.Sample(DiffuseSampler, input.texCoord);
-//#else
-//	float4 col = float4(1.0f, 1.0f, 1.0f, 1.0f);
-//#endif
-//	col = col * float4(diffuseTint.rgb, 1.0f);
-//
-//	return col;
-//}
